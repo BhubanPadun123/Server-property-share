0e74554c49e70c706b2a2cf1619e2680aa8ef2c4
Bhuban <bhubanpadun15m37@gmail.com>
2024-10-22 17:22:51 +0530

    resolved db connection error

--- utils/db_config.js
+++ utils/db_config.js
@@ -2,29 +2,15 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
-let DB = null;
 
-// Use a single pool for both production and development environments
-//mysql://root:nZjKwOsGKFYOsVIRIEzaIqEcouyfGZJl@junction.proxy.rlwy.net:15743/railway
-const urlDB = `mysql://root:nZjKwOsGKFYOsVIRIEzaIqEcouyfGZJl@host:3306/railway`
-if (isProduction) {
-    // DB = mysql.createPool({
-    //     host: process.env.MYSQLHOST,
-    //     port: process.env.MYSQLPORT,
-    //     user: process.env.MYSQLUSER,
-    //     password: process.env.MYSQLPASSWORD,
-    //     database: process.env.MYSQLDATABASE // Use the correct database variable for production
-    // });
-    DB = mysql.createConnection(urlDB);
-} else {
-    DB = mysql.createPool({
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
-}
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 
+const DB = isProduction ? mysql.createConnection(urlDB) : mysql.createPool({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 // Function to test connection (no need to call .connect())
 const connectDB = () => {
     try {
